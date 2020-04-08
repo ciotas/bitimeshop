b6d030a769fa57b495c89fd8014701d8ce15ea52
ciotas <linzikristy@qq.com>
2020-04-08 14:16:01 +0800

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Carbon;
 
 class CouponCodesController extends Controller
 {
-    public function show($code)
+    public function show($code, Request $request)
     {
         if (!$record = CouponCode::where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
         return $record;
     }
 }
